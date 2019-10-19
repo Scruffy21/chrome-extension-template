8c769c635922f671df32e4f5686bdea6a09bba5c
WhyAmI <yougotmebeggingyouformercy@gmail.com>
2019-10-19 18:53:11 +0200

    sendMessageAsync

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,1 +1,8 @@
 const sleep = (time: number) => new Promise((resolve) => window.setTimeout(resolve, time));
+
+// only for non-background pages
+function sendMessageAsync(message: any) {
+    return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage(message, resolve);
+    });
+}
